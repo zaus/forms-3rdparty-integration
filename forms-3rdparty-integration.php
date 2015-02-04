5324c1a11473189d32f5ce3dcb2e4e02d7620f7e
zaus <dr.zaus+dev@gmail.com>
2015-02-04 00:13:05 -0500

    version bump, changelog

--- forms-3rdparty-integration.php
+++ forms-3rdparty-integration.php
@@ -5,7 +5,7 @@ Plugin Name: Forms: 3rd-Party Integration
 Plugin URI: https://github.com/zaus/forms-3rdparty-integration
 Description: Send plugin Forms Submissions (Gravity, CF7, Ninja Forms, etc) to a 3rd-party URL
 Author: zaus, atlanticbt, spkane
-Version: 1.6.3
+Version: 1.6.3.1
 Author URI: http://drzaus.com
 Changelog:
 	1.4 - forked from cf7-3rdparty.  Removed 'hidden field plugin'.
@@ -49,7 +49,7 @@ class Forms3rdPartyIntegration {
 	 * Version of current plugin -- match it to the comment
 	 * @var string
 	 */
-	const pluginVersion = '1.6.2';
+	const pluginVersion = '1.6.3.1';
 
 	
 	/**
